4d78853520b00dd21c851d874418717f760e148a
JayeshKarandeDev <jayesh.karande.dev@gmail.com>
2024-07-07 20:15:28 +0530

    connection code updated to latest

--- configs/setup.go
+++ configs/setup.go
@@ -10,29 +10,38 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func ConnectDB() *mongo.Client {
-	client, err := mongo.NewClient(options.Client().ApplyURI(EnvMongoURI()))
+func ConnectDB() (*mongo.Client, error) {
+	uri := EnvMongoURI()
+	clientOpts := options.Client().ApplyURI(uri)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel() // Ensure the context is canceled when done
+
+	client, err := mongo.Connect(ctx, clientOpts)
 	if err != nil {
 		fmt.Println("Mongo Connection error")
 		log.Fatal(err)
 	}
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err = client.Connect(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
 
-	//ping the database
+	// Ping the database
 	err = client.Ping(ctx, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("Connected to MongoDB")
-	return client
+
+	return client, nil
 }
 
 // Client instance
-var DB *mongo.Client = ConnectDB()
+var DB *mongo.Client
+var err error
+
+func init() {
+	DB, err = ConnectDB()
+	if err != nil {
+		panic(err)
+	}
+}
 
 // getting database collections
 func GetCollection(client *mongo.Client, collectionName string) *mongo.Collection {
